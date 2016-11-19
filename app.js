6ac97cb9e2ec4820b607f825ca4402f4e993e189
David Warren <david@Davids-MacBook-Pro-2.local>
2016-11-19 16:05:58 +0000

    more stuff

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ angular.module('mohack', ['ngRoute'])
 	.config(function ($routeProvider) {
 		$routeProvider.when('/', {templateUrl: 'locations.html', controller: 'LocationsController'});
 		$routeProvider.when('/location/IM-HERE/:id', {templateUrl: 'select.html', controller: 'LocationsSelectController'});
+		$routeProvider.when('/posts/:id', {templateUrl: 'posts.html', controller: 'PostsController'});
 
 	})
 	.controller('LocationsController',function($scope, ApiService, $location) {
@@ -14,53 +15,49 @@ angular.module('mohack', ['ngRoute'])
 		$scope.error = false;
 
 		$scope.selectLocation = function(id) {
-			console.log(id)
-			$location.url('/location/IM-HERE/' + id);
+				$location.url('/location/IM-HERE/' + id);
 		}
+
 		function getLocations() {
-			$scope.locations = ApiService.getLocations();
+			ApiService.getLocations()
+				.then(function(data){
+					$scope.locations = data;
+				})
 		}
 
 		getLocations();
 	})
-	.controller('LocationsSelectController', function($scope, ApiService){
-		
+	.controller('LocationsSelectController', function($scope, ApiService, $location, $routeParams){
+		$scope.enterLocation = function() {
+			$location.url('/posts/' + $routeParams.id);
+		}
+	})
+	.controller('PostsController', function($scope, ApiService, $routeParams) {
+			
+			var id = $routeParams.id;
+			$scope.test = "test";
+
+			function getPosts(id) {
+
+			}
 	})
 	.service('ApiService', function($http, $q) {
-
+		this.username = '';
 		/*
 		*	Function which generates the omdb api url
 		*/
-		var QUOTE_API_ROOT = 'http://movie-quotes-2.herokuapp.com/api/v1/quotes';
+		var QUOTE_API_ROOT = 'http://mohack.herokuapp.com';
 
 		this.getLocations = function() {
-			return [{
-					 id: 1,
-					 online: true,
-					 location: "Cafe Nero, Belfast",
-
-					},
-					{
-					 id: 2,
-					 online: true,
-					 location: "Starbucks, Belfast",
-
-					},
-					{
-					 id: 3,
-					 online: true,
-					 location: "Costa, Belfast",
-
-					},
-
-
-					];
+			return $http.get(QUOTE_API_ROOT + '/GetLocations')
 		}
 		/*
 		*	Function which generates the omdb api url
 		*/
 		
-
+		this.setUserName = function() {
+			this.username = 'USER-' + Math.floor(Math.random()*20+1);
+		}
 		/*
 		* 	Function which gets all of the quotes from the movie quotes api,
 		*	and uses the film property of each json object to query the omdb
